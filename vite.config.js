f43d1ec6a2dd851fbc3aca0a0fe8f06ac1199e09
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-04-27 22:15:42 +0300

    make it work

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000 // Set the chunk size warning limit to 1000 KB (1 MB)
+    chunkSizeWarningLimit: 10000 // Set the chunk size warning limit to 1000 KB (1 MB)
   }
 });
